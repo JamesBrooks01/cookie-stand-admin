f463a9ca8c02c5559066726685b509dad73656f4
James Brooks <brooksjamesk01@gmail.com>
2022-06-08 17:47:00 -0700

    moved header function to file and added overview button

---
+++ components/header.js
@@ -0,0 +1,6 @@
+export default function Header() {
+  return <header className='px-8 py-6 bg-emerald-400'>
+      <h1 className='inline-block text-4xl'>Cookie Stand Admin</h1>
+      <button className="absolute w-20 p-1 bg-white rounded-md left-3/4">Overview</button>
+      </header>
+}
