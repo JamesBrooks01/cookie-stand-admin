01388b531c55d9cb66dfe8975a70ab4d7a0d2e8c
James Brooks <brooksjamesk01@gmail.com>
2022-06-09 21:48:00 -0700

    altered header to fit spec. Added Username display, and a button labeled sign out

--- components/header.js
+++ components/header.js
@@ -1,6 +1,10 @@
-export default function Header() {
-  return <header className='px-8 py-6 bg-emerald-400'>
-      <h1 className='inline-block text-4xl'>Cookie Stand Admin</h1>
-      <button className="absolute w-20 p-1 bg-white rounded-md left-3/4">Overview</button>
+export default function Header({ user }) {
+  return <header className='grid grid-cols-2 grid-rows-1 px-8 py-5 bg-emerald-400'>
+      <h1 className='ml-20 text-4xl'>Cookie Stand Admin</h1>
+      <div className="absolute grid grid-cols-3 grid-rows-1 gap-3 mb-5 left-3/4">
+        <p className="order-1 w-20 p-1 bg-white rounded-md">{user.username}</p>
+        <button className="order-2 w-20 p-1 rounded-md bg-emerald-600">Sign Out</button>
+        <button className="order-3 w-20 p-1 bg-white">Overview</button>
+      </div>
       </header>
 }
